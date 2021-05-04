b3e38bfc98646fe09c586efb4694fe27cf103606
Avinash Kumar <itsmeatg@outlook.com>
2021-05-04 06:08:01 +0530

    Basic File of Our Project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Testing</h1>
+    )
+}
+
+ReactDom.render(<App/>,document.getElementById('root'))
